7bf37b7d67626c5546ad2a53adf8548948ae4be9
Xbladexx <martinezmunozam@gmail.com>
2022-01-05 19:08:21 -0500

    trabajo.cpp

---
+++ C++/trabajo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+float nota1 , nota2 , nota3 , nota4 , total;
+
+int main()
+{
+	cout<<"Ingrese la nota1: ";
+	cin>>nota1;
+	cout<<"Ingrese la nota2: ";
+	cin>>nota2;
+	cout<<"Ingrese la nota3: ";
+	cin>>nota3;
+	cout<<"Ingrese la nota4: ";
+	cin>>nota4;
+
+	total=(nota1+nota2+nota3+nota4)/4
+	cout<<"Su total es: "<<total<<endl;
+	return 0;
+}
