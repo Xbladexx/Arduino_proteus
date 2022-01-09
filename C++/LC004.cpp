2b2e8c789ba30fa75477b0a94ab4e3fab95d537a
Xbladexx <martinezmunozam@gmail.com>
2022-01-09 03:06:36 -0500

    LC004.cpp

---
+++ C++/LC004.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <cstdlib>
+
+using namespace std;
+
+float practica , parcial , final , Promedio ;
+
+char Alumno[100];
+char Curso[100];
+
+int main()
+{
+	system("cls");
+	cout<<"Nombre del alumno: ";
+	cin>>Alumno;
+	cout<<"Nombre del curso: ";
+	cin>>Curso;
+	printf("Promedio de practicas: ");
+	scanf("%f",&practica);
+	printf("Examen parcial: ");
+	scanf("%f",&parcial);
+	printf("Examen final: ");
+	scanf("%f",&final);
+
+	Promedio=((practica*2)+(parcial*1)+(final*3))/6;
+
+	printf("Promedio final es: %1.f \n",Promedio);
+
+	getch();
+	
+}
