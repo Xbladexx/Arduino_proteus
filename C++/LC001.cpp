b365498f7b32b87f9161da8a56f2fd3b5fcdf891
Xbladexx <martinezmunozam@gmail.com>
2022-01-09 02:04:33 -0500

    LC001.cpp

--- C++/LC001.cpp
+++ C++/LC001.cpp
@@ -16,13 +16,13 @@ int main()
 	P=A*B;
 	D=A/B;
 
-	//cout<<S<<endl;
+	
 	cout<<"El resultado de S: "<<S<<endl;
-	//cout<<R<<endl;
+
 	cout<<"El resultado de R: "<<R<<endl;
-	//cout<<P<<endl;
+
 	cout<<"El resultado de P: "<<P<<endl;
-	//cout<<D<<endl;
+
 	cout<<"El resultado de D: "<<D<<endl;
 
 	return 0;
