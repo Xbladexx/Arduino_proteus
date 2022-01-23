83447345824205d6cb936cae209fdcb1671ea85c
Xbladexx <martinezmunozam@gmail.com>
2022-01-22 23:01:27 -0500

    LC024.cpp

---
+++ C++/LC/LC024.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+char nom[100] ;
+int i;
+
+int main()
+{
+    cout<<"Ingresar apellidos y nombres: ";
+    cin>>nom;
+    
+    for (i=1 ; i<=10 ; i++)
+    
+    cout<<nom<<endl;    
+    
+    
+    return 0;
+}
