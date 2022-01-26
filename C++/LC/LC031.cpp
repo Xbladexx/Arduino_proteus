452b1c8686cee0e48f7b21a33b031a8f1da36f36
Xbladexx <martinezmunozam@gmail.com>
2022-01-26 01:25:04 -0500

    LC031.cpp

---
+++ C++/LC/LC031.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int i;
+float suma , promedio , nota , Nmin=20 , Nmax=0 , Nnotas=10;
+int main()
+{
+    for ( i = 1; i < Nnotas; i++)
+    {
+        cout<<"Ingrese nota: ";
+        cin>>nota;
+    
+    if (nota > Nmax)
+    {
+        Nmax = nota;
+    }
+    if (nota < Nmin)
+    {
+        Nmin = nota;
+    }
+
+    
+        suma = suma + nota;
+    }
+        promedio = suma / Nnotas;
+
+        cout<<"Maxima nota: "<<Nmax<<endl;
+        cout<<"Minima nota: "<<Nmin<<endl;
+        cout<<"Promedio: "<<promedio<<endl;
+    
+
+    return 0;
+}
