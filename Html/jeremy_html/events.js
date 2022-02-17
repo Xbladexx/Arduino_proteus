d06c6f7915326da0d52bd77042cf2d29513b589b
Xbladexx <martinezmunozam@gmail.com>
2022-02-16 20:35:53 -0500

    event.js

--- Html/jeremy_html/events.js
+++ Html/jeremy_html/events.js
@@ -1,4 +1,15 @@
 function texto(){
-    let text = document.getElementById("demo1").value;
-    document.getElementById("demo").innerHTML = text;
+	let text = document.getElementById("demo1").value;
+	document.getElementById("demo").innerHTML = text;
+	//window.open("");
+}
+
+function car(){
+	let text = document.getElementById("cars").value;
+	document.getElementById("selecionado").innerHTML = text;
+}
+
+function text_area(){
+	let text = document.getElementById("text_area").value;
+	document.getElementById("text_area1").innerHTML = text;
 }
